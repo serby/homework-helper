9e41f96beb1fba9a2a89e8b38d6501acc4167843
Paul Serby <paul.serby@clock.co.uk>
2015-09-26 16:17:38 +0100

    Get rid of american

--- components/browser/spellings/init.js
+++ components/browser/spellings/init.js
@@ -1,26 +1,31 @@
 var Model = require('./models/spelling')
   , Collection = require('./collections/collection')
   , initController = require('./controllers/controller')
-  , spellings = require('./data/spellings-2015-09-21.json')
+  , spellingGroups =
+    [ require('./data/spellings-2015-09-21.json')
+    , require('./data/spellings-group1.json')
+    , require('./data/spellings-group2.json') ]
+
+  , initSay = require('./lib/say')
 
 function init (serviceLocator) {
 
   var collection = new Collection(serviceLocator)
-  collection.add(new Model(serviceLocator, spellings))
-  initController(serviceLocator, collection)
 
-  window.$('body .js-internal').on('click', function (e) {
-    e.preventDefault()
-    serviceLocator.router(window.$(e.target).attr('href'))
+  spellingGroups.forEach(function (spellings) {
+    collection.add(new Model(serviceLocator, spellings))
   })
 
-  if ('speechSynthesis' in window) {
-    serviceLocator.register('say', require('./lib/say'))
-    serviceLocator.router(document.location.pathname)
-  } else {
-    serviceLocator.router('/unsupported')
-  }
+  initController(serviceLocator, collection)
 
+  initSay((err, say) => {
+    if (err) {
+      serviceLocator.router('/unsupported')
+    } else {
+      serviceLocator.register('say', say)
+      serviceLocator.router(document.location.pathname)
+    }
+  })
 }
 
 module.exports = init
--- components/browser/spellings/lib/say.js
+++ components/browser/spellings/lib/say.js
@@ -1,30 +1,37 @@
-var queue = []
-  , playing = false
-  , msg = new window.SpeechSynthesisUtterance()
-  msg.lang = 'en-GB'
-  msg.voiceURI = 'Google UK English Female'
-  msg.rate = 1
+var iOS9voice =
+    { name: 'Daniel'
+    , voiceURI: 'com.apple.ttsbundle.Daniel-compact'
+    , lang: 'en-GB'
+    , localService: true
+    , 'default': true }
+    , selectedVoice
+    , rate = 1
 
-  msg.onend = () => {
-    playing = false
-    queue.shift()
-    process()
+function createSay (cb) {
+
+  if (!('speechSynthesis' in window)) return cb(new Error('Can not start speech synthesis'))
+  window.speechSynthesis.getVoices()
+
+  setTimeout(() => {
+    var allVoices = window.speechSynthesis.getVoices()
+    , voices = allVoices.filter(voice => { return voice.lang === 'en-GB' })
+    selectedVoice = voices[0]
+    if (selectedVoice === undefined) {
+      selectedVoice = iOS9voice
+      rate = 0.3
+    } else {
+      selectedVoice = voices.filter(voice => { return (/female/i).test(voice.name) })[0] || selectedVoice
+    }
+    cb(null, say)
+  }, 300)
+
+  function say (phrase) {
+    var msg = new window.SpeechSynthesisUtterance(phrase)
+    msg.voice = selectedVoice
+    msg.rate = rate
+    console.log(phrase)
+    window.speechSynthesis.speak(msg)
   }
-
-function say (phrase) {
-
-  if (queue[queue.length - 1] === phrase) return
-  queue.push(phrase)
-  process()
-
 }
 
-function process () {
-  if ((playing) || (queue.length === 0)) return
-  playing = true
-  msg.text = queue[0]
-  console.log('Saying', msg.text)
-  window.speechSynthesis.speak(msg)
-}
-
-module.exports = say
+module.exports = createSay
