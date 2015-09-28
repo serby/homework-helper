3d96cd0f577e856b21b60c7d03b8271d3699ddd9
Paul Serby <paul.serby@clock.co.uk>
2015-09-28 22:31:07 +0100

    Add this weeks spellings

--- components/browser/spellings/init.js
+++ components/browser/spellings/init.js
@@ -2,10 +2,10 @@ var Model = require('./models/spelling')
   , Collection = require('./collections/collection')
   , initController = require('./controllers/controller')
   , spellingGroups =
-    [ require('./data/spellings-2015-09-21.json')
+    [ require('./data/spellings-2015-09-28.json')
+    , require('./data/spellings-2015-09-21.json')
     , require('./data/spellings-group1.json')
     , require('./data/spellings-group2.json') ]
-
   , initSay = require('./lib/say')
 
 function init (serviceLocator) {
