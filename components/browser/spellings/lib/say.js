ea39912e52ee6dbdc907d1578112e24a84ebc292
Paul Serby <paul.serby@clock.co.uk>
2018-05-24 23:39:02 +0100

    Better support for voice synth

--- components/browser/spellings/lib/say.js
+++ components/browser/spellings/lib/say.js
@@ -1,36 +1,23 @@
-var iOS9voice =
-    { name: 'Daniel'
-    , voiceURI: 'com.apple.ttsbundle.Daniel-compact'
-    , lang: 'en-GB'
-    , localService: true
-    , 'default': true }
-    , selectedVoice
-    , defaultRate = 0.9
-
 function createSay (cb) {
 
   if (!('speechSynthesis' in window)) return cb(new Error('Can not start speech synthesis'))
   window.speechSynthesis.getVoices()
 
-  setTimeout(() => {
-    var allVoices = window.speechSynthesis.getVoices()
-    , voices = allVoices.filter(voice => { return voice.lang === 'en-GB' })
-    selectedVoice = voices[0]
-    if (selectedVoice === undefined) {
-      selectedVoice = iOS9voice
-    } else {
-      selectedVoice = voices.filter(voice => { return (/female/i).test(voice.name) })[0] || selectedVoice
-    }
-    cb(null, say)
-  }, 500)
-
   function say (phrase, rate) {
-    var msg = new window.SpeechSynthesisUtterance(phrase)
+    //alert(window.speechSynthesis.getVoices())
+    var allVoices = window.speechSynthesis.getVoices()
+    , voices = allVoices.filter(voice => voice.lang === 'en-GB')
+    , msg = new window.SpeechSynthesisUtterance(phrase)
+    , selectedVoice = voices.find(v => v.name.match(/female/i))
+
+    if (!selectedVoice) selectedVoice = voices[0]
     msg.voice = selectedVoice
-    msg.rate = rate || defaultRate
+    msg.rate = 0.85
     console.log(phrase)
     window.speechSynthesis.speak(msg)
   }
+
+  cb(null, say)
 }
 
 module.exports = createSay
