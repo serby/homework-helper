90c72a64c7579c11934ad8dedd7b44e4d274adf5
Paul Serby <paul.serby@clock.co.uk>
2018-05-24 23:38:34 +0100

    New responses

--- components/browser/spellings/lib/phrase-maker.js
+++ components/browser/spellings/lib/phrase-maker.js
@@ -5,6 +5,7 @@ var allPhrases =
     , 'Very good!'
     , 'Boom boom boom!'
     , 'Correct!'
+    , 'Oh! You are very good at this!'
     , 'Wooo! yay! That is right!'
     , 'Yep, good job!'
     , 'You\'re right, well done' ]
@@ -13,10 +14,12 @@ var allPhrases =
     , 'Not quite right buddy!'
     , 'Sorry that\'s not right'
     , 'Wrong I\'m affraid'
+    , 'Ummm, think again dude!'
     , 'Oh dear, I think you made a mistake.' ]
   , wellDone:
     [ 'Nice job! You should get a prize.'
     , 'Well done! You\'re really clever.'
+    , 'Yeah! Whoop whoop yay!'
     , 'Good work buddy! You\'re a master speller.'
     , 'Give me five dude, you got them all right!'
     ]
