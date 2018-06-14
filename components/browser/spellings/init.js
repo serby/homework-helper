8568ab0ca14a9494290372cb639fdf6a7edd2cbe
Paul Serby <paul.serby@clock.co.uk>
2018-06-14 10:29:46 +0100

    Add Greta's spellings

--- components/browser/spellings/init.js
+++ components/browser/spellings/init.js
@@ -7,6 +7,7 @@ var Model = require('./models/spelling')
     , require('./data/spellings-year4-summer2-week3')
     , require('./data/spellings-year4-summer2-week4')
     , require('./data/spellings-year1-4-june-2018.json')
+    , require('./data/spellings-year1-14-june-2018.json')
     ]
   , initSay = require('./lib/say')
 
